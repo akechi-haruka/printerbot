d41040f252f5290bf377d7cb1caf3e55f83d8306
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2024-07-04 17:59:28 +0200

    remove paths from dist

--- dist.bat
+++ dist.bat
@@ -2,16 +2,6 @@
 cd %~dp0
 mkdir dist
 
-copy buildDir\chcfwdl\chcfwdl.dll L:\SDVX\kantai7\C310FWDLusb.dll
-copy buildDir\chcusb\chcusb.dll L:\SDVX\kantai7\C310Ausb.dll
-copy buildDir\chcfwdl\chcfwdl.dll W:\apm\BZ10\App\C310FWDLusb.dll
-copy buildDir\chcusb\chcusb.dll W:\apm\BZ10\App\C310Ausb.dll
-copy buildDir\chcusb330\chcusb330.dll L:\SDVX\fatego10\C330Ausb.dll
-copy buildDir\chcfwdl\chcfwdl.dll L:\SDVX\fatego10\C330AFwdlUsb.dll
-copy buildDir\chcusb330\chcusb330.dll W:\apm\EJ10\App\C330Ausb.dll
-copy buildDir\chcusb\chcusb.dll W:\apm\EJ10\App\chcusb.dll
-copy buildDir\chcfwdl\chcfwdl.dll W:\apm\EJ10\App\C330AFwdlUsb.dll
-
 copy buildDir\chcfwdl\chcfwdl.dll dist\CXXXFwdlusb.dll
 copy buildDir\chcusb\chcusb.dll dist\CXXXusb.dll
 copy buildDir\chcusb330\chcusb330.dll dist\C330Ausb.dll
